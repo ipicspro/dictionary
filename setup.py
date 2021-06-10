b8e83c0281d225b75237a3068b45e9a36d9fc0df
ipicspro <36360306+ipicspro@users.noreply.github.com>
2021-06-10 17:55:02 +0300

    depend

--- setup.py
+++ setup.py
@@ -2,16 +2,18 @@ from setuptools import setup, find_packages
 
 
 setup(name='dictionary',
-      version='0.1',
-      url='https://github.com/ipicspro/dictionary',
-      license='MIT',
-      author='IS',
-      author_email='info@ecommaker.com',
-      description='Dictionary',
-      #packages=find_packages(exclude=['tests']),
-      packages=['dictionary'],
-      long_description=open('README.md').read(),
-      zip_safe=False,
-      setup_requires=['nose', 'emoji'],
-      test_suite='nose.collector')
+    version='0.1',
+    url='https://github.com/ipicspro/dictionary',
+    license='MIT',
+    author='IS',
+    author_email='info@ecommaker.com',
+    description='Dictionary',
+    #packages=find_packages(exclude=['tests']),
+    packages=['dictionary'],
+    long_description=open('README.md').read(),
+    zip_safe=False,
+    setup_requires=['nose'],
+#   dependency_links=['emoji'],
+    install_requires=['emoji'],
+    test_suite='nose.collector')
 
